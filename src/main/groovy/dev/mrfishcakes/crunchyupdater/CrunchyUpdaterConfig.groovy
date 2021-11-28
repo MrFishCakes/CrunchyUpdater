31913b10d49cca580a748b172b822dae3646f7a8
MrFishCakes <finlayoxby@outlook.com>
2021-11-28 00:17:42 +0000

    Add JavaDoc

--- src/main/groovy/dev/mrfishcakes/crunchyupdater/CrunchyUpdaterConfig.groovy
+++ src/main/groovy/dev/mrfishcakes/crunchyupdater/CrunchyUpdaterConfig.groovy
@@ -2,12 +2,46 @@ package dev.mrfishcakes.crunchyupdater
 
 import org.gradle.api.provider.Property
 
+/**
+ * Interface for defining values within configuration block
+ *
+ * @author MrFishCakes
+ */
 interface CrunchyUpdaterConfig {
 
+    /**
+     * Header to be printed before commits
+     *
+     * @return Header message
+     */
     Property<String> getHeader()
+
+    /**
+     * GitHub account that owns the repository
+     *
+     * @return Repository owner
+     */
     Property<String> getRepoOwner()
+
+    /**
+     * Name of the repository
+     *
+     * @return Repository name
+     */
     Property<String> getRepoName()
+
+    /**
+     * Old git commit hash to compare from
+     *
+     * @return Old git hash
+     */
     Property<String> getOldHash()
+
+    /**
+     * New git commit hash to compare against
+     *
+     * @return New git hash
+     */
     Property<String> getNewHash()
 
 }
